89e7b066fbd0abbf3bdc340ce6d562e070fd4a89
Arman <grigoryan.arman@bk.ru>
2019-09-22 21:05:38 +0400

    Add slider animation part

--- slider.js
+++ slider.js
@@ -1,8 +1,8 @@
 $.fn.slider = function (options) {
     this.each(function () {
         const target = $(this);
-        const { slideShow = false, interval = 5000, animation } = options;
-        let current = 0;
+        const { slideShow = false, interval = 5000, animation = 'slide'} = options;
+        let current = 0, animationStarted = false, animate;
         let elements = target.children().map((index, el) => {
             return {
                 type: el.dataset.type,
@@ -11,14 +11,15 @@ $.fn.slider = function (options) {
         });
         let slider = $('<div class="sliderContainer"></div>'),
             sliderElements = $('<div class="sliderElements"></div>'),
-            sliderCircles = $('<div class="sliderCircles"></div>');
+            sliderCircles = $('<div class="sliderCircles"></div>'),
+            left, right;
 
         elements.each(i => {
            let element = elements[i], content, play_pause,
                active = current === i ? 'active' : '',
                activeElement = current === i ? 'activeElement' : '',
                div = $(`<div class="content ${activeElement}"></div>`),
-               circle = $(`<div class="circle ${active}"></div>`);
+               circle = $(`<div class="circle ${active}"  data-el="${i}"></div>`);
 
            switch (element.type) {
                case 'image':
@@ -27,8 +28,25 @@ $.fn.slider = function (options) {
                case 'video':
                    content = $(`<video src="${element.url}"></video>`);
                    play_pause = $('<div class="video"></div>');
+                   content.click(playVideo);
+                   play_pause.click(playVideo);
+
+                   function playVideo() {
+                       if (!play_pause.hasClass('playing')) {
+                           if (animationStarted) return;
+                           div.stop();
+                           content.get(0).play();
+                           clearInterval(animate)
+                       }
+                       else {
+                           content.get(0).pause();
+                           checkAnimate(slideShow)
+                       }
+                       play_pause.toggleClass('playing');
+                   }
 
                    div.append(play_pause);
+                   break;
            }
 
            sliderCircles.append(circle);
@@ -36,7 +54,76 @@ $.fn.slider = function (options) {
            sliderElements.append(div);
         });
 
-        slider.append(sliderElements, sliderCircles);
+        sliderCircles.on('click', '.circle', function () {
+            checkAnimate(slideShow);
+            changeSlide(parseInt(this.dataset.el, 10));
+        });
+
         target.after(slider).remove();
-    })
+
+        if (elements.length > 1) {
+            left = $('<div class="leftArrow"></div>');
+            right = $('<div class="rightArrow"></div>');
+
+            left.click(() => {
+                current = parseInt(sliderCircles.find('.active').get(0).dataset.el, 10);
+                let leftElement = current === 0 ? elements.length - 1 : current - 1;
+                checkAnimate(slideShow);
+                changeSlide(leftElement);
+            });
+            right.click(() => {
+                checkAnimate(slideShow);
+                changeElement(current);
+            });
+        }
+
+        slider.append(left, sliderElements, right, sliderCircles);
+
+        if(slideShow) animate = setInterval(goOn, interval);
+
+        function changeSlide(current) {
+            let activeVideo = sliderElements.find('.activeElement .video');
+            if(activeVideo.length && activeVideo.hasClass('playing')) activeVideo.next().get(0).pause();
+            animationStarted = true;
+            sliderElements.find('.activeElement').animate({
+                opacity: 0,
+            }, interval / 10, 'linear', function () {
+                $(this).removeClass('activeElement');
+
+                sliderElements.find('.content').eq(current).animate({
+                    opacity: 1,
+                    display: 'flex'
+                }, interval / 10, 'linear',function () {
+                    animationStarted = false;
+                    changeActive(current);
+                }).addClass('activeElement');
+            });
+        }
+
+        function changeActive(i) {
+            let circle = sliderCircles.find('.circle');
+            circle.removeClass('active');
+            circle.eq(i).addClass('active');
+        }
+
+        function changeElement(current) {
+            current = parseInt(sliderCircles.find('.active').get(0).dataset.el, 10);
+            let rightElement = current === elements.length - 1 ? 0 : current + 1;
+            changeSlide(rightElement);
+        }
+
+        function goOn() {
+            changeElement(parseInt(sliderCircles.find('.circle.active').get(0).dataset.el), 10);
+        }
+
+        function checkAnimate(slideShow) {
+            if (slideShow) {
+                clearInterval(animate);
+                sliderElements.find('.content').stop();
+                animate = setInterval(goOn, interval);
+            }
+        }
+    });
+
+    return this;
 };
